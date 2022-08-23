e083cb7cf9be7d6fcc59247b662791c5881b854a
srock2580 <srock2580@gmail.com>
2022-08-23 15:59:56 +0530

    update

--- light on off/script.js
+++
@@ -1,6 +0,0 @@
-"use strict"; // javascript goes here...
-
-document.querySelector('.power-button').addEventListener('click', function() {
-  document.querySelector('.light-bulb-on').classList.toggle('inactive');
-  document.querySelector('.light-bulb-off').classList.toggle('inactive');
-});
