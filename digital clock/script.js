e5e0996ccd962d95bcfe415e04fdd217353995a1
srock2580 <srock2580@gmail.com>
2022-09-11 21:56:37 +0530

    update

--- digital clock/script.js
+++ digital clock/script.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+setInterval(() => {
+  let hour = new Date().getHours()
+  let minute = new Date().getMinutes()
+  let second = new Date().getSeconds()
+
+  document.querySelector('.hour').innerText = hour
+  document.querySelector('.minute').innerText = minute
+  document.querySelector('.second').innerText = second
+}, 500);
