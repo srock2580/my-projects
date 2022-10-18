ef16a6fe5af57e6f21bc098b04a73e494248c5f7
srock2580 <srock2580@gmail.com>
2022-10-18 17:37:16 +0530

    update

---
+++ to do list/todo-app/src/component/todoinput.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function TodoInput({addItemFunction}) {
+  const [text, setText] = React.useState("");
+
+  const getInput = (event) => {
+    event.preventDefault();
+    setText(event.target.value);
+  };
+
+  const addItem = (event) => {
+    event.preventDefault()
+    addItemFunction(text)
+    setText('')
+  };
+  return (
+    <form className="input-sec">
+      <input type="text" value={text} placeholder="Add Task" onChange={getInput} />
+      <input type="submit" value="ADD" onClick={addItem} />
+    </form>
+  );
+}
+
+export { TodoInput };
